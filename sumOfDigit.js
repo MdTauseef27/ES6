4f82cbdcb2ce19c5baf24a98e9abdb7f971ef78a
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-16 23:15:43 +0530

    changes in the code

--- sumOfDigit.js
+++ sumOfDigit.js
@@ -1,12 +1,12 @@
 const funct1 = () => {
-  let n = parseInt(111);
+  let n = 999;
   let m;
   let sum = 0;
   while (n > 0) {
     m = n % 10;
     sum = sum + m;
-    n = n / 10;
+    n = Math.floor(n / 10);
   }
-  console.log("Sum Of Digit", sum);
+  console.log("Sum Of Digit", parseInt(sum));
 };
 funct1();
