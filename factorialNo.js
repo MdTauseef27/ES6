e76962317fe72688f25013dcfd0a0aabbe9ab396
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-16 11:10:48 +0530

    changes

--- factorialNo.js
+++ factorialNo.js
@@ -1,8 +1,8 @@
 const factorialNo = () => {
   let fact = 1;
-  let number = 5;
-  let i;
-  for (i = 1; i <= number; i++) {
+  let number = 4;
+  //   let i;
+  for (let i = 1; i <= number; i++) {
     fact = fact * i;
   }
   console.log("Factorial Number=", fact);
