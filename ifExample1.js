8152d9f0699abe0677be553f458e47ea7255882c
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-16 11:36:21 +0530

    added some code

--- ifExample1.js
+++ ifExample1.js
@@ -1,10 +1,11 @@
 const f1 = () => {
-  a = 5;
+  a = 50;
   b = 10;
+
+  c = 1 - "1";
+  console.log("addition=", c);
   if (a > b) {
     console.log("a is greater than b");
-  } else {
-    console.log("b is greater than a");
   }
 };
 f1();
