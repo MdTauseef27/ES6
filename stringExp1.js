acc9329601ff1a17d543ef9c4ce6143bfa7b3308
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-17 23:28:07 +0530

    added the eplace method

--- stringExp1.js
+++ stringExp1.js
@@ -1,8 +1,11 @@
 const stringExp = () => {
   let str = "Apple,Banana,Kiwi";
+  let strname = "Please Visit Microsoft";
   let str1 = str.slice(6, 12); // this method extacts the part of sting and return the extracted part in string
   let str2 = str.substring(13, 17); //same as slice but not accept negative values
+  let str3 = strname.replace("Microsoft", "Google"); //it will replace microsoft to google
   console.log(str1);
   console.log(str2);
+  console.log(str3);
 };
 stringExp();
