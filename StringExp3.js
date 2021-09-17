2697ecf107caeec3451b641ebdcbc1e1fa76a8c7
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-17 22:59:08 +0530

    checking typeof datatype

--- StringExp3.js
+++ StringExp3.js
@@ -1,6 +1,6 @@
 const stringExp3 = () => {
-  let firstName = "John";
-  let lastName = new String("Cena");
-  console.log(firstName + lastName);
+  let firstName = "John"; ///This is Sting
+  let lastName = new String("Cena"); ///This is Object
+  console.log(firstName + lastName + typeof firstName + typeof lastName);
 };
 stringExp3();
