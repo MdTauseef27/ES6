7510bc681284908255d48ef66d9e061f32914397
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-17 23:24:21 +0530

    added the comment line

--- stringExp1.js
+++ stringExp1.js
@@ -1,6 +1,8 @@
 const stringExp = () => {
   let str = "Apple,Banana,Kiwi";
-  let str1 = str.slice(6, 12);
+  let str1 = str.slice(6, 12); // this method extacts the part of sting and return the extracted part in string
+  let str2 = str.substring(13, 17); //same as slice but not accept negative values
   console.log(str1);
+  console.log(str2);
 };
 stringExp();
