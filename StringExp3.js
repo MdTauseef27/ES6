549803d3b26118172e368f9d065d81a25778a1a7
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-17 23:14:07 +0530

    changes

--- StringExp3.js
+++ StringExp3.js
@@ -1,6 +1,6 @@
 const stringExp3 = () => {
-  let firstName = "John"; ///This is Sting
+  let firstName = "John"; ///This is String
   let lastName = new String("Cena"); ///This is Object
-  console.log(firstName + lastName + typeof firstName + typeof lastName);
+  console.log(firstName + lastName + typeof firstName + typeof lastName); //typeof is used to check the data type
 };
 stringExp3();
