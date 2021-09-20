6caf0963a15eaec3cd681c5488caa1e300fb9a71
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-20 12:23:45 +0530

    added some text

--- stringExp1.js
+++ stringExp1.js
@@ -1,6 +1,6 @@
 const stringExp = () => {
   let str = "Apple,Banana,Kiwi,Mango";
-  let strname = "Please Visit Microsoft";
+  let strname = "Please Visit Microsoft.com Web Page";
   let str1 = str.slice(6, 12); // this method extacts the part of sting and return the extracted part in string
   let str2 = str.substring(13, 17); //same as slice but not accept negative values
   let str3 = strname.replace("Microsoft", "Google"); //it will replace microsoft to google
