6d54be7d9eb1182513c97b7885799ff4fae953ca
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-21 11:11:19 +0530

    comment the reassigning code

--- vaiableDecl.js
+++ vaiableDecl.js
@@ -2,7 +2,7 @@ const funct1 = () => {
   //   var x = 14;
   //   let x = 14;
   const x = 14;
-  x = 12; //reassigning a const vaiable will give an eror
+  // x = 12; //reassigning a const vaiable will give an eror
   console.log(x);
 };
 funct1();
