abc44844c637b1b197dd716d52d34116ec8fee41
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-09-21 11:10:42 +0530

    eassigning a const vaiable will give an eor

--- vaiableDecl.js
+++ vaiableDecl.js
@@ -1,7 +1,8 @@
 const funct1 = () => {
   //   var x = 14;
-  let x = 14;
-  x = 12;
+  //   let x = 14;
+  const x = 14;
+  x = 12; //reassigning a const vaiable will give an eror
   console.log(x);
 };
 funct1();
